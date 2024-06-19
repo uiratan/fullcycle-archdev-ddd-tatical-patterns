de8b0ecfd2c26284c2b4aeb707934b82e5d18b6a
Uiratan Cavalcante <uiratan@gmail.com>
2024-06-19 18:37:52 -0300

    test runner config

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const address = new Address("Street 1", 1, "00000-000", "City");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 10);
-const item2 = new OrderItem("2", "Item 2", 15);
+const item1 = new OrderItem("1", "Item 1", 10, "p1", 2);
+const item2 = new OrderItem("2", "Item 2", 15, "p2", 1);
 const order = new Order("1", "1", [item1, item2]);
 
