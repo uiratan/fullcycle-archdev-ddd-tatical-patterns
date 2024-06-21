7b5c5d65f6013a4add6780e95d2f6b4fbbb2db54
Uiratan Cavalcante <uiratan@gmail.com>
2024-06-21 16:42:07 -0300

    customer reward points test

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -57,4 +57,15 @@ describe("Customer unit tests", () => {
 
     });
 
+    it("should add reward points", () => {  
+        const customer = new Customer("1", "John");        
+        expect(customer.rewardPoints).toBe(0);
+        
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(10);     
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(20);
+    });
+
 });
